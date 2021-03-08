772e0fc02a13cb514e1304face263eb34efe90dc
Ashleigh Kasha <akasha@va-b5.kzn.21-school.ru>
2021-03-08 20:23:31 +0300

    добавил функицю ft_strcmp.c к либе

---
+++ src/utils/libft/ft_strcmp.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: user <user@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/11 20:47:05 by user              #+#    #+#             */
+/*   Updated: 2021/02/11 21:17:16 by user             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_strcmp(const char *s1, const char *s2)
+{
+	int	i;
+
+	i = 0;
+	if (!s1 && !s2)
+		return (0);
+	while (1)
+	{
+		if (s1[i] == '\0' || s2[i] == '\0')
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+		if (s1[i] != s2[i])
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+		i++;
+	}
+	return (0);
+}
--- src/utils/libft/includes/libft.h
+++ src/utils/libft/includes/libft.h
@@ -6,7 +6,7 @@
 /*   By: akasha <akasha@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/28 18:36:22 by akasha            #+#    #+#             */
-/*   Updated: 2020/12/06 19:48:36 by akasha           ###   ########.fr       */
+/*   Updated: 2021/03/08 20:21:29 by akasha           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,6 +38,7 @@ int					ft_isalpha(int c);
 int					ft_atoi(const char *str);
 
 int					ft_strncmp(const char *s1, const char *s2, size_t n);
+int					ft_strcmp(const char *s1, const char *s2);
 char				*ft_strnstr(const char *haystack, const char *needle,
 					size_t len);
 size_t				ft_strlcpy(char *dst, const char *src, size_t dstsize);
