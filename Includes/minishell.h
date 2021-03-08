b210283c810507184c07ca396a5b7c61552b45cd
Ashleigh Kasha <akasha@va-b5.kzn.21-school.ru>
2021-03-08 20:53:01 +0300

    добавил поле procces_name в структуру data

--- Includes/minishell.h
+++ Includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: akasha <akasha@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/08 16:01:35 by akasha            #+#    #+#             */
-/*   Updated: 2021/03/08 20:18:30 by akasha           ###   ########.fr       */
+/*   Updated: 2021/03/08 20:28:03 by akasha           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,7 @@ typedef struct	s_data
 {
 	char		**env;
 	char		**argv;
+	char		*procces_name;
 }				t_data;
 
 
