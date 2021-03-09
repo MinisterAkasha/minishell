3306bc9fbccdd6a370e094b4014bd3996066c21f
Ashleigh Kasha <akasha@va-b5.kzn.21-school.ru>
2021-03-09 13:15:00 +0300

    добавил прототип структуры данных для хранения информации о исполняемой функции

--- Includes/minishell.h
+++ Includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: akasha <akasha@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/08 16:01:35 by akasha            #+#    #+#             */
-/*   Updated: 2021/03/08 20:28:03 by akasha           ###   ########.fr       */
+/*   Updated: 2021/03/09 13:12:35 by akasha           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,5 +32,11 @@ typedef struct	s_data
 	char		*procces_name;
 }				t_data;
 
+typedef struct	s_exe_info
+{
+	int			(*exe_function)();
+	int			(*operator_exe_function)();
+	char		**args;
+}				t_exe_info;
 
 #endif
