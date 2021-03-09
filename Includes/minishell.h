9bd048fd65fdaf11ec8bdf2d740db084a780e9fc
Ashleigh Kasha <akasha@va-b5.kzn.21-school.ru>
2021-03-09 19:22:12 +0300

    добавил функцию освобождающая память двумерного массива

--- Includes/minishell.h
+++ Includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: akasha <akasha@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/08 16:01:35 by akasha            #+#    #+#             */
-/*   Updated: 2021/03/09 19:01:54 by akasha           ###   ########.fr       */
+/*   Updated: 2021/03/09 19:21:14 by akasha           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,5 +41,12 @@ int		launch_shell(t_data *data, char **args);
 ** init.c
 */
 void	init_support_parsing_arr(t_parser_data *data);
+
+/*
+** init.c
+*/
+void	free_2d_arr(char **arr);
+
+
 
 #endif
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: akasha <akasha@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/08 16:01:06 by akasha            #+#    #+#             */
-/*   Updated: 2021/03/09 19:01:54 by akasha           ###   ########.fr       */
+/*   Updated: 2021/03/09 19:20:41 by akasha           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,12 +40,11 @@ int launch_shell(t_data *data, char **args)
 	return (1);
 }
 
-
 int loop_shell(t_data *data)
 {
-	char *line;
-	char **args;
-	int status;
+	char			*line;
+	char			**args;
+	int				status;
 	t_parser_data	support;
 
 	status = 1;
@@ -57,6 +56,7 @@ int loop_shell(t_data *data)
 		args = ft_split(line, ' ');
 		status = execute(data, args, support);
 		free(line);
+		free_2d_arr(args);
 		free(args);
 	}
 	return (1);
