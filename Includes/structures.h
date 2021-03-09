6110592fcebca7db85027a614cc5bfe2ab9e4380
Ashleigh Kasha <akasha@va-b5.kzn.21-school.ru>
2021-03-09 14:53:39 +0300

    переименовал operator в oper в структуре exe_info

--- Includes/structures.h
+++ Includes/structures.h
@@ -6,12 +6,20 @@
 /*   By: akasha <akasha@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/09 14:40:03 by akasha            #+#    #+#             */
-/*   Updated: 2021/03/09 14:40:40 by akasha           ###   ########.fr       */
+/*   Updated: 2021/03/09 14:52:48 by akasha           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef STRUCTURES_H
 # define STRUCTURES_H
+
+typedef struct	s_exe_info
+{
+	int			(*exe_function)(char **args);
+	int			(*operator_exe_function)(char **args);
+	char		**args;
+	char		*oper;
+}				t_exe_info;
 
 typedef struct	s_data
 {
@@ -20,12 +28,6 @@ typedef struct	s_data
 	char		*procces_name;
 }				t_data;
 
-typedef struct	s_exe_info
-{
-	int			(*exe_function)(char **args);
-	int			(*operator_exe_function)(char **args);
-	char		**args;
-	char		*operator;
-}				t_exe_info;
+
 
 #endif
