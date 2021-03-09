ae2339581c4364767853f48ae8d173d52aa8975d
Ashleigh Kasha <akasha@va-b5.kzn.21-school.ru>
2021-03-09 17:10:38 +0300

    добавил проверку пустого инпута

--- src/execute/execute.c
+++ src/execute/execute.c
@@ -6,7 +6,7 @@
 /*   By: akasha <akasha@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/09 13:22:26 by akasha            #+#    #+#             */
-/*   Updated: 2021/03/09 15:11:43 by akasha           ###   ########.fr       */
+/*   Updated: 2021/03/09 16:52:28 by akasha           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ int	execute(t_data *data, char **args)
 {
 	t_exe_info	info;
 
+	if (!args[0])
+		return (1);
 	info = parser(args);
 	info.exe_function(info.args);
 	if (!ft_strcmp(info.oper, ""))
