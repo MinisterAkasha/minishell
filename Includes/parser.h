9064f0ff163a463ca2708bf7e2bfc724e50e85c0
Ashleigh Kasha <akasha@va-b5.kzn.21-school.ru>
2021-03-09 16:48:04 +0300

    добавил хедер для парсера

--- Includes/parser.h
+++ Includes/parser.h
@@ -1,41 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   minishell.h                                        :+:      :+:    :+:   */
+/*   parser.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: akasha <akasha@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/03/08 16:01:35 by akasha            #+#    #+#             */
-/*   Updated: 2021/03/09 16:44:21 by akasha           ###   ########.fr       */
+/*   Created: 2021/03/09 16:43:25 by akasha            #+#    #+#             */
+/*   Updated: 2021/03/09 16:45:44 by akasha           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MINISHELL_H
-# define MINISHELL_H
-
-
-# include <stdlib.h>
-# include <unistd.h>
-# include <stdio.h>
-# include <sys/errno.h>
-
-# include <sys/types.h>
-# include <sys/wait.h>
+#ifndef PARSER_H
+# define PARSER_H
 
 # include "structures.h"
 
-# include "execute.h"
-# include "parser.h"
-
-# include "get_next_line.h"
-# include "../src/utils/libft/includes/libft.h"
-
+int			exe_cd(char **args);
+int			exe_pwd(char **args);
+int			exe_echo(char **args);
+int			exe_export(char **args);
+int			exe_unset(char **args);
+int			exe_env(char **args);
+int			exe_exit(char **args);
+int			unknown_command(char **args);
 
 t_exe_info	parser(char **args);
-/*
-** loop_shell
-*/
-void wait_child_process_end(pid_t id);
-int launch_shell(t_data *data, char **args);
 
 #endif
