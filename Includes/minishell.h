0dd78633455b63786350d54be3a9e70bdb927402
Ashleigh Kasha <akasha@va-b5.kzn.21-school.ru>
2021-03-09 15:14:01 +0300

    добавил прототип parser в minishell.h

--- Includes/minishell.h
+++ Includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: akasha <akasha@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/08 16:01:35 by akasha            #+#    #+#             */
-/*   Updated: 2021/03/09 14:41:29 by akasha           ###   ########.fr       */
+/*   Updated: 2021/03/09 15:11:34 by akasha           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,14 @@
 
 # include "get_next_line.h"
 # include "../src/utils/libft/includes/libft.h"
+# include "execute.h"
 
+
+t_exe_info	parser(char **args);
+/*
+** loop_shell
+*/
+void wait_child_process_end(pid_t id);
+int launch_shell(t_data *data, char **args);
 
 #endif
