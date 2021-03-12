8b477be20e5c4b99207c2ec13a8d501711309fe6
Ashleigh Kasha <akasha@va-b5.kzn.21-school.ru>
2021-03-12 17:07:11 +0300

    добавил посик бинарников по относительному пути

--- src/execute/execute.c
+++ src/execute/execute.c
@@ -6,7 +6,7 @@
 /*   By: akasha <akasha@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/09 13:22:26 by akasha            #+#    #+#             */
-/*   Updated: 2021/03/12 14:27:44 by akasha           ###   ########.fr       */
+/*   Updated: 2021/03/12 17:05:55 by akasha           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,13 +26,15 @@ int	execute(t_data *data, char **args, t_support_parsing_data support)
 	if (!args[0])
 		return (1);
 	info = parser(args, support);
-	bin_exe_path = search(info.args[0], get_env_param("PATH", data->env));
 	if (info.exe_function)
 		info.exe_function(info.args, data->env);
 	else if (bin_exe_path)
+	{
+		bin_exe_path = search(info.args[0], get_env_param("PATH", data->env));
 		launch_shell(data, args, bin_exe_path);
+		free(bin_exe_path);
+	}
 	else
 		unknown_command(info.args, data->env);
-	free(bin_exe_path);
 	return (1);
 }
--- src/search/search.c
+++ src/search/search.c
@@ -6,7 +6,7 @@
 /*   By: akasha <akasha@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/11 22:12:44 by akasha            #+#    #+#             */
-/*   Updated: 2021/03/12 16:16:57 by akasha           ###   ########.fr       */
+/*   Updated: 2021/03/12 17:05:43 by akasha           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,12 +47,12 @@ static int check_dir(char *path, char *arg)
 char	*get_command_from_path(char *path)
 {
 	char **splited_path;
-	char *comand;
+	char *command;
 
 	splited_path = ft_split(path, '/');
-	comand = ft_strdup(splited_path[get_arr_length(splited_path) - 1]);
+	command = ft_strdup(splited_path[get_arr_length(splited_path) - 1]);
 	free_2d_arr(splited_path);
-	return (comand);
+	return (command);
 }
 
 static char *get_bin_path_command(char *arg, const char *$_path)
@@ -60,26 +60,57 @@ static char *get_bin_path_command(char *arg, const char *$_path)
 	char	**bin_paths;
 	char	*current_path;
 	char	*with_slash;
-	char	*comand;
+	char	*command;
 	int		i = 0;
 
-	comand = get_command_from_path(arg);
+	command = get_command_from_path(arg);
 	bin_paths = ft_split(ft_strchr($_path, '=') + 1, ':');
 	current_path = NULL;
 	while (bin_paths[i])
 	{
-		with_slash = ft_strjoin(bin_paths[i], "/");
-		if (check_dir(bin_paths[i], comand))
+		if (check_dir(bin_paths[i], command))
 		{
-			current_path = ft_strjoin(with_slash, comand);
+			with_slash = ft_strjoin(bin_paths[i], "/");
+			current_path = ft_strjoin(with_slash, command);
+			free(with_slash);
 			break;
 		}
-		free(with_slash);
 		i++;
 	}
-	free(comand);
-	free(with_slash);
+	free(command);
 	free_2d_arr(bin_paths);
+	return (current_path);
+}
+
+char *get_dir_path(char *pwd, char *arg)
+{
+	char	*dir_path;
+	char	*arg_path;
+
+	arg_path = ft_substr(arg, 0, ft_strlen(arg) - ft_strlen(ft_strrchr(arg, '/')));
+	dir_path = ft_strjoin(pwd, arg_path);
+	free(arg_path);
+	return (dir_path);
+}
+
+char	*get_relative_path(char *arg)
+{
+	char	*current_path;
+	char	*command;
+	char	pwd[2048];
+	char	*dir_path;
+	char	*pwd_with_slash;
+
+	getcwd(pwd, 2048);
+	current_path = NULL;
+	command = get_command_from_path(arg);
+	pwd_with_slash = ft_strjoin(pwd, "/");
+	dir_path = get_dir_path(pwd_with_slash, arg);
+	if (check_dir(dir_path, command))
+		current_path = ft_strjoin(pwd_with_slash, arg);
+	free(command);
+	free(dir_path);
+	free(pwd_with_slash);
 	return (current_path);
 }
 
@@ -88,7 +119,7 @@ char *search(char *arg, const char *$_path)
 	char *current_path;
 
 	current_path = get_bin_path_command(arg, $_path);
-	// if (!current_path)
-	// 	current_path = get_relative_path(arg);
+	if (!current_path)
+		current_path = get_relative_path(arg);
 	return (current_path);
 }
