b260e04a2ee04005083dc6b9a9438e4515223973
Ashleigh Kasha <akasha@va-b5.kzn.21-school.ru>
2021-03-12 13:29:41 +0300

    Добавил функцию поиска команды по папкам из /Users/akasha/.brew/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/munki:/Users/akasha/.brew/bin

---
+++ src/search/search.c
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   search.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: akasha <akasha@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/11 22:12:44 by akasha            #+#    #+#             */
+/*   Updated: 2021/03/12 13:29:05 by akasha           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+static int	check_dir(char *path, char *arg)
+{
+	DIR *dir;
+    struct dirent *entry;
+
+    dir = opendir(path);
+    if (!dir) {
+        printf("Can't open");//TODO
+    };
+	entry = readdir(dir);
+    while (entry != NULL)
+	{
+		entry = readdir(dir));
+		if (!ft_strcmp(arg, entry->d_name))
+			return (1);
+    };
+    closedir(dir);
+	return (0);
+}
+
+char		*search(char *arg, const char *$_path)
+{
+	char	**bin_paths;
+	char	*current_path;
+	char	*with_slash;
+	int		i = 0;
+
+	bin_paths = ft_split(ft_strchr($_path, '=') + 1, ':');
+	while (bin_paths[i])
+	{
+		if (check_dir(bin_paths[i], arg))
+		{
+			with_slash = ft_strjoin(bin_paths[i], "/");
+			current_path = ft_strjoin(with_slash, arg);
+			free(with_slash);
+			return (current_path);
+		}
+		i++;
+	}
+	free_2d_arr(bin_paths);
+	return (NULL);
+}
