97eaa99dc18f5ae1a02d34b4d807f6e9978a140f
Ashleigh Kasha <akasha@va-b5.kzn.21-school.ru>
2021-03-13 23:05:31 +0300

    закоментировал добавление перемнных

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: akasha <akasha@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/08 16:01:06 by akasha            #+#    #+#             */
-/*   Updated: 2021/03/13 23:00:19 by akasha           ###   ########.fr       */
+/*   Updated: 2021/03/13 23:05:11 by akasha           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -69,11 +69,10 @@ int main(int argc, char **argv, char **env)
 	store->input.env = create_env(env);//env;
 	store->input.procces_name = argv[0];
 	store->variables = NULL;
-	add_variable_to_list(&store->variables, "name", "Ilya", 0);
-	add_variable_to_list(&store->variables, "id", "1", 0);
-	add_variable_to_list(&store->variables, "status", "ok", 0);
-	t_variable *variable = find_variable(store->variables, "name");
-	printf("%s: %s\n", variable->key, variable->value);
+	// add_variable_to_list(&store->variables, "name", "Ilya", 0);
+	// add_variable_to_list(&store->variables, "id", "1", 0);
+	// add_variable_to_list(&store->variables, "status", "ok", 0);
+	// t_variable *variable = find_variable(store->variables, "name");
 	loop_shell(store);
 	free_2d_arr(store->input.env);
 	return (0);
