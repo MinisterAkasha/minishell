c3f5cc7360eea3edcb9408b42ef985e6bd9d96eb
Ashleigh Kasha <akasha@va-b5.kzn.21-school.ru>
2021-03-13 22:13:42 +0300

    переименовал функцию

--- src/shlvl/shlvl.c
+++ src/shlvl/shlvl.c
@@ -6,7 +6,7 @@
 /*   By: akasha <akasha@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/13 18:19:30 by akasha            #+#    #+#             */
-/*   Updated: 2021/03/13 18:54:20 by akasha           ###   ########.fr       */
+/*   Updated: 2021/03/13 19:38:36 by akasha           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,6 @@ void	shlvl(char **env)
 
 	current_shlvl = ft_atoi(ft_strchr(get_env_param("SHLVL", env), '=') + 1);
 	lvl = ft_itoa(current_shlvl + 1);
-	change_env(lvl, "SHLVL", &env);
+	change_env_value(lvl, "SHLVL", &env);
 	free(lvl);
 }
