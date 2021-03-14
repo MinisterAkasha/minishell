3d07866d696163036bc9d2b3a236b31cba4215d3
pichkasik <sasha.marakulin@mail.ru>
2021-03-14 16:37:05 +0300

    Норама +, кроме одной строки

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -53,14 +53,14 @@ int			unknown_command(char **args, char **env)
 	return (1);
 }
 
-static	char get_separator(char *arg)
+static	char	get_separator(char *arg)
 {
 	int		i;
 	char	separator;
 
 	i = 0;
 	separator = 'f';
-	while(arg[i] && separator == 'f')
+	while (arg[i] && separator == 'f')
 	{
 		if (arg[i] == '"' || arg[i] == '\'')
 			separator = arg[i];
@@ -69,7 +69,13 @@ static	char get_separator(char *arg)
 	return (separator);
 }
 
-static	void cut_separator(char **arg, char separator)
+static	void		ft_costil(int *i, char **first)
+{
+	*i = 0;
+	*first = 0;
+}
+
+static	void	cut_separator(char **arg, char separator)
 {
 	int		i;
 	int		len;
@@ -77,8 +83,7 @@ static	void cut_separator(char **arg, char separator)
 	char	*second;
 	int		start_index;
 
-	i = 0;
-	first = 0;
+	ft_costil(&i, &first);
 	if (separator == (*arg)[0])
 		i++;
 	while (1)
@@ -88,7 +93,7 @@ static	void cut_separator(char **arg, char separator)
 		while ((*arg)[i] != separator && (*arg)[i++])
 			len++;
 		if (first != 0)
-			break;
+			break ;
 		first = ft_substr((*arg), start_index, len);
 		i++;
 	}
@@ -99,49 +104,46 @@ static	void cut_separator(char **arg, char separator)
 	free(second);
 }
 
-static	void concat_arg(t_exe_info	**exe_info, char *arg)
+static	void	concat_arg(t_exe_info **exe_info, char *arg)
 {
-	char	separator;
-	char	*copy_exe_arg;
-	t_exe_info	*copy_exe_info;
+	char		separator;
+	char		*copy_exe_arg;
+	t_exe_info	*tmp_lst;
 
-	copy_exe_info = *exe_info;
+	tmp_lst = *exe_info;
 	separator = get_separator(arg);
-	copy_exe_arg = ft_strdup(copy_exe_info->arg);
-	free(copy_exe_info->arg);
+	copy_exe_arg = ft_strdup(tmp_lst->arg);
+	free(tmp_lst->arg);
 	if (separator == 'f')
-		copy_exe_info->arg = ft_strjoin(copy_exe_arg, arg); // check if first arg is "" or NULL
+		tmp_lst->arg = ft_strjoin(copy_exe_arg, arg);
 	else
 	{
 		cut_separator(&arg, separator);
-		copy_exe_info->arg = ft_strjoin(copy_exe_arg, arg);
+		tmp_lst->arg = ft_strjoin(copy_exe_arg, arg);
 	}
 	free(arg);
 	free(copy_exe_arg);
 }
 
-static	int init_exec_func(t_exe_info	**exe_info, t_support_parsing_data support, char *arg)
+static	void	init_exec_func(t_exe_info **exe_info,
+								t_support_parsing_data support, char *arg)
 {
 	int				i;
-	char	*str;
-	t_exe_info	*copy_exe_info;
+	t_exe_info		*tmp_lst;
 
-	copy_exe_info = *exe_info;
+	tmp_lst = *exe_info;
 	cut_separator(&arg, get_separator(arg));
 	i = 0;
 	while (i < sizeof(support.exe_str_arr) / sizeof(char *))
 	{
 		if (!ft_strcmp(arg, support.exe_str_arr[i]))
-			copy_exe_info->exe_function = support.exe_func_arr[i];
+			tmp_lst->exe_function = support.exe_func_arr[i];
 		i++;
 	}
 	free(arg);
-	if (!copy_exe_info->exe_function)
-		return (0);
-	return (1);
 }
 
-static	void	create_new_list(t_exe_info **lst)
+static	void	set_default_new_lst(t_exe_info **lst)
 {
 	(*lst)->next = NULL;
 	(*lst)->exe_function = NULL;
@@ -149,7 +151,8 @@ static	void	create_new_list(t_exe_info **lst)
 	(*lst)->arg = ft_strdup("");
 }
 
-static	int init_oper_exec_func(t_exe_info **copy_exe_info, t_support_parsing_data support, char *arg)
+static	int		init_operator(t_exe_info **tmp_lst, int *increm,
+								t_support_parsing_data support, char *arg)
 {
 	int		i;
 
@@ -158,118 +161,72 @@ static	int init_oper_exec_func(t_exe_info **copy_exe_info, t_support_parsing_dat
 	{
 		if (!ft_strcmp(arg, support.operators_arr[i]))
 		{
-			if (!((*copy_exe_info)->next = (t_exe_info *)malloc(sizeof(t_exe_info))))
+			if (!((*tmp_lst)->next = (t_exe_info *)malloc(sizeof(t_exe_info))))
 				return (0);
-			(*copy_exe_info) = (*copy_exe_info)->next;
-			create_new_list(copy_exe_info);
-			(*copy_exe_info)->operator_exe_function = support.operators_exe_func_arr[i];
-			return (i);
+			(*tmp_lst) = (*tmp_lst)->next;
+			set_default_new_lst(tmp_lst);
+			(*tmp_lst)->operator_exe_function = support.operators_exe_func_arr[i];
+			*increm += 1;
+			return (1);
 		}
 		i++;
 	}
 	return (-1);
 }
 
-int		get_exe_info(char **args, t_store *store)
+int				get_exe_info(char **args, t_store *store)
 {
-	t_exe_info	*copy_exe_info;
-	size_t		i;
-	int			index_oper;
+	t_exe_info	*tmp_lst;
+	int			i;
 
 	i = 0;
-	if (!(store->exe_info = (t_exe_info *)malloc(sizeof (t_exe_info))))
+	if (!(store->exe_info = (t_exe_info *)malloc(sizeof(t_exe_info))))
 		return (0);
-	create_new_list(&store->exe_info);
-	copy_exe_info = store->exe_info;
-	while(args[i])
+	set_default_new_lst(&store->exe_info);
+	tmp_lst = store->exe_info;
+	while (args[i])
 	{
-		index_oper = -1;
-		init_exec_func(&copy_exe_info, store->support, args[i]);
-		while(args[i])
+		init_exec_func(&tmp_lst, store->support, args[i]);
+		while (args[i])
 		{
-			i++;
-			if (!args[i])
+			if (!args[++i] ||
+				(init_operator(&tmp_lst, &i, store->support, args[i])) > 0)
 				break ;
-			if ((index_oper = init_oper_exec_func(&copy_exe_info, store->support, args[i])) >= 0)
-			{
-				i++;
-				break ;
-			}
 			else
-				concat_arg(&copy_exe_info, args[i]);
+				concat_arg(&tmp_lst, args[i]);
 		}
 		i++;
 	}
 	return (1);
 }
 
-int main()
-{
-	t_exe_info *test;
-	t_exe_info *fucking_test;
-	char *str = "  echo 111'111' | cd papka > echo \"222\"222 >> 'echo' \"333333\" ;    echo    '' |  echo 444444";
-	char **splited_str;
-	t_store *store;
-
-	if (!(store = (t_store *)malloc(sizeof(t_store))))
-		return (0);
-	init_support_parsing_arr(&store->support);
-	splited_str = split(str);
-	if (!(get_exe_info(splited_str, store)))
-		return (0);
-
-//	while (1)
-//	{
-//
-//	}
-	while(store->exe_info)
-	{
-		printf("\n====$$$$====\n");
-		printf("store->exe_info->arg: %s\n",store->exe_info->arg);
-		printf("store->exe_info->exe_function: %p\n",store->exe_info->exe_function);
-		printf("store->exe_info->operator_exe_function: %p\n",store->exe_info->operator_exe_function);
-		printf("\n");
-		store->exe_info = store->exe_info->next;
-	}
-	return (0);
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//
-//t_exe_info	parser(char **args, t_support_parsing_data support)
+//int main()
 //{
-//	t_exe_info		info;
-//	int				i;
+//	t_exe_info *test;
+//	t_exe_info *fucking_test;
+//	char *str = "  echo 111'111' | cd papka > echo \"222\"222 >> 'echo' \"333333\" ;    echo    '' |  echo 444444";
+//	char **splited_str;
+//	t_store *store;
 //
-//	i = 0;
-//	info.exe_function = NULL;
-//	info.operator_exe_function = NULL;
-//	info.args = args;
-//	while (i < sizeof(support.exe_str_arr) / sizeof(char *))
+//	if (!(store = (t_store *)malloc(sizeof(t_store))))
+//		return (0);
+//	init_support_parsing_arr(&store->support);
+//	splited_str = split(str);
+//	if (!(get_exe_info(splited_str, store)))
+//		return (0);
+//
+////	while (1)
+////	{
+////
+////	}
+//	while(store->exe_info)
 //	{
-//		if (!ft_strcmp(args[0], support.exe_str_arr[i]))
-//			info.exe_function = support.exe_func_arr[i];
-//		i++;
+//		printf("\n====$$$$====\n");
+//		printf("store->exe_info->arg: %s\n",store->exe_info->arg);
+//		printf("store->exe_info->exe_function: %p\n",store->exe_info->exe_function);
+//		printf("store->exe_info->operator_exe_function: %p\n",store->exe_info->operator_exe_function);
+//		printf("\n");
+//		store->exe_info = store->exe_info->next;
 //	}
-//	if (!info.exe_function)
-//		info.exe_function = unknown_command;
-//	info.oper = "";
-//	return (info);
+//	return (0);
 //}
