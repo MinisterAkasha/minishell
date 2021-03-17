7c63bef5c09e3260de459702a226b4e2835d5c03
Ashleigh Kasha <akasha@va-b5.kzn.21-school.ru>
2021-03-17 19:42:24 +0300

    норминет сортировки export

--- src/execute/export/sort_export.c
+++ src/execute/export/sort_export.c
@@ -6,13 +6,13 @@
 /*   By: akasha <akasha@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/14 12:54:57 by akasha            #+#    #+#             */
-/*   Updated: 2021/03/17 12:53:59 by akasha           ###   ########.fr       */
+/*   Updated: 2021/03/17 19:41:47 by akasha           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int partition(char **export, int left, int right)
+int		partition(char **export, int left, int right)
 {
 	int i;
 	int j;
@@ -26,12 +26,12 @@ int partition(char **export, int left, int right)
 		while (export[j][k] <= export[right][k])
 		{
 			if (export[j][k] == '=' && export[right][k] == '=')
-				break;
+				break ;
 			if (export[j][k] < export[right][k])
 			{
 				swap_elems(&export[i], &export[j]);
 				i++;
-				break;
+				break ;
 			}
 			k++;
 		}
@@ -41,7 +41,7 @@ int partition(char **export, int left, int right)
 	return (i);
 }
 
-void swap_elems(char **elem_1, char **elem_2)
+void	swap_elems(char **elem_1, char **elem_2)
 {
 	char *swap;
 
@@ -50,7 +50,7 @@ void swap_elems(char **elem_1, char **elem_2)
 	*elem_2 = swap;
 }
 
-char **sort_export(char **export, int left, int right)
+char	**sort_export(char **export, int left, int right)
 {
 	int pivot;
 
@@ -60,6 +60,5 @@ char **sort_export(char **export, int left, int right)
 		sort_export(export, left, pivot - 1);
 		sort_export(export, pivot + 1, right);
 	}
-
 	return (export);
 }
