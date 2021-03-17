a967ea861dcc04e20298f027847c84d2f5340ce2
Ashleigh Kasha <akasha@va-b5.kzn.21-school.ru>
2021-03-17 14:50:04 +0300

    добавил функцию заполнения листа переменных из введеных значений

--- src/execute/export/export.c
+++ src/execute/export/export.c
@@ -6,24 +6,17 @@
 /*   By: akasha <akasha@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/13 23:09:30 by akasha            #+#    #+#             */
-/*   Updated: 2021/03/17 14:44:39 by akasha           ###   ########.fr       */
+/*   Updated: 2021/03/17 14:49:36 by akasha           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int exe_export(t_exe_args *exe_arg)
+void	fill_variable_list(t_exe_args *exe_arg)
 {
-	char **export;
 	char **variable;
-
-	// export = create_env(exe_arg->env);
-	t_variable *$variable; // = find_variable(variables, "name");
-	// printf("%s: %s\n", variable->key, variable->value);
-	//TODO цикл по agrs и создать переменные
 	int i = 1;
-	add_variable_to_list(&exe_arg->variables, "a", "1", 1);
-	add_variable_to_list(&exe_arg->variables, "b", "2", 1);
+	
 	while (exe_arg->args[i])
 	{
 		variable = ft_split(exe_arg->args[i], '=');
@@ -36,9 +29,19 @@ int exe_export(t_exe_args *exe_arg)
 		free(variable);
 		i++;
 	}
+}
+
+int exe_export(t_exe_args *exe_arg)
+{
+	char **export;
+	
+
+	// export = create_env(exe_arg->env);
+	fill_variable_list(exe_arg);
+	
 
 	t_list *tmp;
-
+	t_variable *$variable;
 	tmp = exe_arg->variables;
 	while (tmp)
 	{
