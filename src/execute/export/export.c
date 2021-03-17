eaca54b0f4b9358cae8631ca9b5bb8fee626c393
Ashleigh Kasha <akasha@va-b5.kzn.21-school.ru>
2021-03-17 14:46:45 +0300

    добавил заполнение листа переменных через export

--- src/execute/export/export.c
+++ src/execute/export/export.c
@@ -6,7 +6,7 @@
 /*   By: akasha <akasha@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/13 23:09:30 by akasha            #+#    #+#             */
-/*   Updated: 2021/03/17 12:39:44 by akasha           ###   ########.fr       */
+/*   Updated: 2021/03/17 14:44:39 by akasha           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,50 @@
 
 int exe_export(t_exe_args *exe_arg)
 {
-	char	**export;
+	char **export;
+	char **variable;
 
-	export = create_env(exe_arg->env);
-	//t_variable *variable; // = find_variable(variables, "name");
+	// export = create_env(exe_arg->env);
+	t_variable *$variable; // = find_variable(variables, "name");
 	// printf("%s: %s\n", variable->key, variable->value);
 	//TODO цикл по agrs и создать переменные
-	if (get_arr_length(exe_arg->args) == 1)
+	int i = 1;
+	add_variable_to_list(&exe_arg->variables, "a", "1", 1);
+	add_variable_to_list(&exe_arg->variables, "b", "2", 1);
+	while (exe_arg->args[i])
 	{
-		sort_export(export, 0, get_arr_length(export) - 1);
-		int i = 0;
-		while (export[i])
-		{
-			printf("%s\n", export[i]);
-			i++;
-		}
+		variable = ft_split(exe_arg->args[i], '=');
+		if (!ft_strchr(exe_arg->args[i], '='))
+			add_variable_to_list(&exe_arg->variables, variable[0], "", 1);
+		else if (variable[1])
+			add_variable_to_list(&exe_arg->variables, variable[0], variable[1], 1);
+		else
+			add_variable_to_list(&exe_arg->variables, variable[0], "\"\"", 1);
+		free(variable);
+		i++;
 	}
+
+	t_list *tmp;
+
+	tmp = exe_arg->variables;
+	while (tmp)
+	{
+		$variable = tmp->content;
+		printf("%s:%s\n", $variable->key, $variable->value);
+		tmp = tmp->next;
+	}
+
+	// while ()
+	// if (get_arr_length(exe_arg->args) == 1)
+	// {
+	// 	sort_export(export, 0, get_arr_length(export) - 1);
+	// 	int i = 0;
+	// 	while (export[i])
+	// 	{
+	// 		printf("%s\n", export[i]);
+	// 		i++;
+	// 	}
+	// }
 
 	return (1);
 }
