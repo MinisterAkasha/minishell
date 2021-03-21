601744f28680acd772f033a8cd42d4838852d0ed
Ashleigh Kasha <akasha@va-b5.kzn.21-school.ru>
2021-03-21 17:21:02 +0300

    добавил рбработку ошибок для cd и результат cd в 0

--- src/execute/cd.c
+++ src/execute/cd.c
@@ -6,7 +6,7 @@
 /*   By: akasha <akasha@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/09 21:43:13 by akasha            #+#    #+#             */
-/*   Updated: 2021/03/20 20:59:51 by akasha           ###   ########.fr       */
+/*   Updated: 2021/03/21 17:20:08 by akasha           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,23 @@ int		exe_cd(t_exe_args *exe_arg)
 	int		res;
 	char	old_pwd[2048];
 	char	new_pwd[2048];
+	char	*error;
 
 	getcwd(old_pwd, 2048);
 	res = chdir(exe_arg->args[0]);
 	if (res == -1)
-		write_error_message("minishell: cd: ", exe_arg->args[0], ": Not a directory"); //TODO доработать ошибки
+	{
+		add_variable_to_list(&exe_arg->variables, "?", "1", 0, 0);
+		error = ft_strjoin(": ", strerror(errno));
+		write_error_message("minishell: cd: ", exe_arg->args[0], error); //TODO доработать ошибки
+		free(error);
+	}
 	else
 	{
 		getcwd(new_pwd, 2048);
 		change_env_value(old_pwd, "OLDPWD", &exe_arg->env);
 		change_env_value(new_pwd, "PWD", &exe_arg->env);
+		add_variable_to_list(&exe_arg->variables, "?", "0", 0, 0);
 	}
 	return (1);
 }
