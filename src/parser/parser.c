dec1fe6f44417d9b2080823eee2c0e15f69c007e
pichkasik <sasha.marakulin@mail.ru>
2021-03-22 16:09:15 +0300

    Исправиление: операнды могут работать без пробелов

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -45,42 +45,17 @@ static	int		init_redirection(t_exe_info **tmp_lst, t_support_parsing_data suppor
 	return (0);
 }
 
-//static	void	init_exe_env(t_exe_info **tmp_lst, t_support_parsing_data support, int *decrement, char *str)
-//{
-//	char	**splited_str;
-//	int		result;
-//	int		i;
-//
-//	result = 1;
-//	i = 0;
-//	splited_str = protect_ft_split(ft_split(str, '='));
-//	if (get_arr_length(splited_str) < 2)
-//		result = 0;
-//	while (splited_str[0][i])
-//	{
-//		if (!ft_isalnum(splited_str[0][i]))
-//			result = 0;
-//		i++;
-//	}
-//	if (result == 1)
-//	{
-//		(*tmp_lst)->exe_function = support.exe_func_arr[7];
-//		init_arg(tmp_lst, str, decrement);
-//	}
-//	free_2d_arr(splited_str);
-//}
-
 static	void	init_exec_func(t_exe_info **exe_info,
 								t_support_parsing_data support, char **args, int *i)
 {
 	int				j;
 	t_exe_info		*tmp_lst;
 	char			*str_to_compare;
-	int				state_env;
 
-	state_env = 0;
 	tmp_lst = *exe_info;
-	str_to_compare = get_str_to_compare(args, i, &state_env);
+	if (args[*i] && !ft_strcmp(args[*i], " "))
+		*i += 1;
+	str_to_compare = get_str_to_compare(args, i);
 	j = 0;
 	while (j < sizeof(support.exe_str_arr) / sizeof(char *))
 	{
@@ -90,12 +65,8 @@ static	void	init_exec_func(t_exe_info **exe_info,
 	}
 	if (init_redirection(&tmp_lst, support, i, str_to_compare))
 		init_arg(&tmp_lst, str_to_compare, i);
-//	if (!tmp_lst->exe_function && state_env == 4)
-//		init_exe_env(&tmp_lst, support, i, str_to_compare);
-//	if (!tmp_lst->exe_function)
-//	{
-//		init_arg(&tmp_lst, str_to_compare, i);
-//	}
+	else if (args[*i] && !ft_strcmp(args[*i], " "))
+		*i += 1;
 	free(str_to_compare);
 }
 
@@ -110,16 +81,15 @@ static	int	init_operator(t_exe_info **tmp_lst, int *increm,
 		if (!ft_strcmp(arg, support.operators_arr[i]))
 		{
 			if (!((*tmp_lst)->next = (t_exe_info *)malloc(sizeof(t_exe_info))))
-				return (0);
+				error_malloc();
 			(*tmp_lst) = (*tmp_lst)->next;
 			set_default_new_lst(tmp_lst);
 			(*tmp_lst)->operator_exe_function = support.operators_exe_func_arr[i];
-			*increm += 1;
-			return (1);
+			return (0);
 		}
 		i++;
 	}
-	return (-1);
+	return (1);
 }
 
 int	get_exe_info(char **args, t_store *store)
@@ -135,13 +105,10 @@ int	get_exe_info(char **args, t_store *store)
 	while (args[i])
 	{
 		init_exec_func(&tmp_lst, store->support, args, &i);
-		while (args[i])
+		while (args[i] && init_operator(&tmp_lst, &i, store->support, args[i]))
 		{
-			if (!args[++i]
-				 || (init_operator(&tmp_lst, &i, store->support, args[i])) > 0)
-				break ;
-			else
-				concat_arg(&tmp_lst, store->support, args[i]);
+			concat_arg(&tmp_lst, store->support, args[i]);
+			i++;
 		}
 		if (!args[i])
 			break ;
@@ -152,6 +119,7 @@ int	get_exe_info(char **args, t_store *store)
 
 //int main()
 //{
+//	char *str = "echo ch'l'en;bin;ls ;'e'c'h'o pam> ty't'y ;e'ch'o 111'111'| cd papka ; echo \"222\"222 >> 'echo' \"333333\" ;    echo    '' | echo 44'44'44 ; echo some_word > test.txt test test ; echo next_word > extra_test.txt extra extra ";
 //	t_exe_info *test;
 //	t_exe_info *fucking_test;
 //	//"name= fsd" -> OK
@@ -161,7 +129,6 @@ int	get_exe_info(char **args, t_store *store)
 //	//"name==kklkf" -> OK
 //	//"name===fdsa" -> OK
 //	//"na'm'e=test" -> OK
-//	char *str = "echo ch'l'en ; bin ; ls ; 'e'c'h'o pam > ty't'y ; e'ch'o 111'111' | cd papka ; echo \"222\"222 >> 'echo' \"333333\" ;    echo    '' | echo 44'44'44 ; echo some_word > test.txt test test ; echo next_word > extra_test.txt extra extra ";
 //	//char *str = "name==kklkf ; name=fasdf'fasdf' ; name='fdsa'sfda ;  name=ppp'fds'=mmmm";
 ////	char *str = "  ";
 //	char **splited_str;
