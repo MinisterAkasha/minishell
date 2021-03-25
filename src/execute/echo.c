14348f1e2e72ed525847428923ee1dd32c6702ad
Ashleigh Kasha <akasha@va-b5.kzn.21-school.ru>
2021-03-25 18:27:57 +0300

    добавил небольшую обработку -n для echo

--- src/execute/echo.c
+++ src/execute/echo.c
@@ -6,7 +6,7 @@
 /*   By: akasha <akasha@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/21 20:58:44 by akasha            #+#    #+#             */
-/*   Updated: 2021/03/23 13:53:41 by akasha           ###   ########.fr       */
+/*   Updated: 2021/03/25 18:27:35 by akasha           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,22 @@
 int			exe_echo(t_exe_args *exe_args)
 {
 	int i;
+	int	flag_n;
 	
 	i = 0;
+	flag_n = !ft_strcmp(exe_args->args[i], "-n");
+	if (flag_n)
+		i = 1;
 	while (exe_args->args[i])
 	{
 		ft_putstr_fd(exe_args->args[i++], 1);
-		ft_putstr_fd(" ", 1);
+		if (exe_args->args[i + 1])
+			ft_putstr_fd(" ", 1); 
 	}
-	ft_putendl_fd("", 1);
+	if (!flag_n)
+		ft_putendl_fd("", 1);
+	else
+		ft_putendl_fd("%", 1);
 	add_variable_to_list(&exe_args->variables, "?", "0", 0, 0);
 	return (1);
 }
