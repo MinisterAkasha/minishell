5a17045df78999aa2925126fb95879d588b5d0c5
Ashleigh Kasha <akasha@va-b5.kzn.21-school.ru>
2021-03-29 14:59:56 +0300

    добавил переход в HOME при использовании cd без аргументов

--- src/execute/cd.c
+++ src/execute/cd.c
@@ -6,11 +6,22 @@
 /*   By: akasha <akasha@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/09 21:43:13 by akasha            #+#    #+#             */
-/*   Updated: 2021/03/21 17:40:37 by akasha           ###   ########.fr       */
+/*   Updated: 2021/03/29 14:58:55 by akasha           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+int		cd_home(char *home)
+{
+	int		res;
+	char	**home_arr;
+
+	home_arr = ft_split(home, '=');
+	res = chdir(home_arr[1]);
+	free_2d_arr(home_arr);
+	return (res);
+}
 
 int		exe_cd(t_exe_args *exe_args)
 {
@@ -20,7 +31,10 @@ int		exe_cd(t_exe_args *exe_args)
 	char	*error;
 
 	getcwd(old_pwd, 2048);
-	res = chdir(exe_args->args[0]);
+	if (!exe_args->args[0])
+		res = cd_home(get_env_param("HOME", exe_args->env));
+	else
+		res = chdir(exe_args->args[0]);
 	if (res == -1)
 	{
 		add_variable_to_list(&exe_args->variables, "?", "1", 0, 0);
