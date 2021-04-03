7745f0d6f4f4a77f1b6411373f30d4f0a28489b6
Ashleigh Kasha <akasha@va-b5.kzn.21-school.ru>
2021-04-03 15:16:51 +0300

    перенес и разбил функцию handle_pipe_command

---
+++ src/operators/pipe/handle_pipe_command.c
@@ -0,0 +1,97 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   handle_pipe_command.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: akasha <akasha@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/03 15:01:20 by akasha            #+#    #+#             */
+/*   Updated: 2021/04/03 15:15:51 by akasha           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+void	close_unused_fd(int **fd, int index[2])
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (fd[i])
+	{
+		j = 0;
+		while (fd[i][j])
+		{
+			if (fd[i][j] != index[0] && fd[i][j] != index[1])
+				close(fd[i][j]);
+			j++;
+		}
+		i++;
+	}
+}
+
+void	handle_start_pipe_command(int **fd, int i)
+{
+	int	index[2];
+
+	index[0] = fd[i][1];
+	index[1] = -1;
+	close_unused_fd(fd, index);
+	dup2(fd[i][1], 1);
+	close(fd[i][1]);
+}
+
+void	handle_center_pipe_command(int **fd, int i)
+{
+	int	index[2];
+
+	index[0] = fd[i - 1][0];
+	index[1] = fd[i][1];
+	close_unused_fd(fd, index);
+	dup2(fd[i - 1][0], 0);
+	dup2(fd[i][1], 1);
+	close(fd[i - 1][0]);
+	close(fd[i][1]);
+}
+
+void	handle_end_pipe_command(int **fd, int i)
+{
+	int	index[2];
+
+	i--;
+	index[0] = fd[i][0];
+	index[1] = -1;
+	close_unused_fd(fd, index);
+	dup2(fd[i][0], 0);
+	close(fd[i][0]);
+}
+
+void	handle_pipe_command(int **fd, t_exe_info *exe_info, t_exe_args *exec_args, int i)
+{
+	int			old_stdout;
+	int			old_stdin;
+	char		*bin_path;
+
+	old_stdout = dup(1);
+	old_stdin = dup(0);
+	bin_path = search(exec_args->args[0], get_env_param("PATH", exec_args->env));
+	if (i == 0)
+		handle_start_pipe_command(fd, i);
+	else if (i == get_int_arr_length(fd))
+		handle_end_pipe_command(fd, i);
+	else
+		handle_center_pipe_command(fd, i);
+	if (exe_info->exe_function)
+		run_exe_function(exe_info, exec_args);
+	else if (bin_path)
+		execve(bin_path, exec_args->args, exec_args->env);
+	else
+	{
+		unknown_command(exec_args);
+		exit(127);
+	}
+	dup2(old_stdout, 1);
+	dup2(old_stdin, 0);
+	free(bin_path);
+}
--- src/operators/pipe/pipe.c
+++ src/operators/pipe/pipe.c
@@ -6,7 +6,7 @@
 /*   By: akasha <akasha@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/24 15:46:31 by akasha            #+#    #+#             */
-/*   Updated: 2021/04/03 14:59:37 by akasha           ###   ########.fr       */
+/*   Updated: 2021/04/03 15:09:22 by akasha           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,71 +56,52 @@ void	run_exe_function(t_exe_info	*exe_info, t_exe_args *exec_args)
 	exit(ft_atoi(var->value));
 }
 
-void	close_unused_fd(int **fd, int index[2])
-{
-	int	i;
-	int	j;
+// void	handle_pipe_command(int **fd, t_exe_info *exe_info, t_exe_args *exec_args, int i)
+// {
+// 	int			old_stdout;
+// 	int			old_stdin;
+// 	char		*bin_path;
+// 	int			index[2];
 
-	i = 0;
-	while (fd[i])
-	{
-		j = 0;
-		while (fd[i][j])
-		{
-			if (fd[i][j] != index[0] && fd[i][j] != index[1])
-				close(fd[i][j]);
-			j++;
-		}
-		i++;
-	}
-}
+// 	old_stdout = dup(1);
+// 	old_stdin = dup(0);
+// 	bin_path = search(exec_args->args[0], get_env_param("PATH", exec_args->env));
+// 	if (i == 0)
+// 	{
+// 		index[0] = fd[i][1];
+// 		index[1] = -1;
+// 		close_unused_fd(fd, index);
+// 		dup2(fd[i][1], 1);
+// 		close(fd[i][1]);
+// 	}
+// 	else if (i == get_int_arr_length(fd))
+// 	{
+// 		i--;
+// 		index[0] = fd[i][0];
+// 		index[1] = -1;
+// 		close_unused_fd(fd, index);
+// 		dup2(fd[i][0], 0);
+// 		close(fd[i][0]);
+// 	}
+// 	else
+// 	{
+// 		index[0] = fd[i - 1][0];
+// 		index[1] = fd[i][1];
+// 		close_unused_fd(fd, index);
+// 		dup2(fd[i - 1][0], 0);
+// 		dup2(fd[i][1], 1);
+// 		close(fd[i - 1][0]);
+// 		close(fd[i][1]);
+// 	}
 
-void	handle_pipe_command(int **fd, t_exe_info *exe_info, t_exe_args *exec_args, int i)
-{
-	int			old_stdout;
-	int			old_stdin;
-	char		*bin_path;
-	int			index[2];
-
-	old_stdout = dup(1);
-	old_stdin = dup(0);
-	bin_path = search(exec_args->args[0], get_env_param("PATH", exec_args->env));
-	if (i == 0)
-	{
-		index[0] = fd[i][1];
-		index[1] = -1;
-		close_unused_fd(fd, index);
-		dup2(fd[i][1], 1);
-		close(fd[i][1]);
-	}
-	else if (i == get_int_arr_length(fd))
-	{
-		i--;
-		index[0] = fd[i][0];
-		index[1] = -1;
-		close_unused_fd(fd, index);
-		dup2(fd[i][0], 0);
-		close(fd[i][0]);
-	}
-	else
-	{
-		index[0] = fd[i - 1][0];
-		index[1] = fd[i][1];
-		close_unused_fd(fd, index);
-		dup2(fd[i - 1][0], 0);
-		dup2(fd[i][1], 1);
-		close(fd[i - 1][0]);
-		close(fd[i][1]);
-	}
-
-	if (exe_info->exe_function)
-		run_exe_function(exe_info, exec_args);
-	else if (bin_path)
-		execve(bin_path, exec_args->args, exec_args->env);
-	dup2(old_stdout, 1);
-	dup2(old_stdin, 0);
-	free(bin_path);
-}
+// 	if (exe_info->exe_function)
+// 		run_exe_function(exe_info, exec_args);
+// 	else if (bin_path)
+// 		execve(bin_path, exec_args->args, exec_args->env);
+// 	dup2(old_stdout, 1);
+// 	dup2(old_stdin, 0);
+// 	free(bin_path);
+// }
 
 int		*create_child_processes(int pipe_num, t_list *info, t_exe_args *exec_args, int **fd)
 {
