6d1175b94914d721108c05bff75275e50f0e1cfb
Ashleigh Kasha <akasha@va-b5.kzn.21-school.ru>
2021-04-03 16:47:14 +0300

    добавил обработку cd без аргумента, если сделать unset HOME

--- src/execute/cd.c
+++ src/execute/cd.c
@@ -6,7 +6,7 @@
 /*   By: akasha <akasha@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/09 21:43:13 by akasha            #+#    #+#             */
-/*   Updated: 2021/03/29 15:36:41 by akasha           ###   ########.fr       */
+/*   Updated: 2021/04/03 16:46:21 by akasha           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,17 @@ int		cd_home(char *home)
 	int		res;
 	char	**home_arr;
 
-	home_arr = protect_ft_split(ft_split(home, '='));
-	res = chdir(home_arr[1]);
-	free_2d_arr(home_arr);
+	if (!home)
+	{
+		write_error_message("minishell: ", "cd", "HOME not set");
+		res = -2;
+	}
+	else
+	{
+		home_arr = protect_ft_split(ft_split(home, '='));
+		res = chdir(home_arr[1]);
+		free_2d_arr(home_arr);
+	}
 	return (res);
 }
 
@@ -28,7 +36,6 @@ int		exe_cd(t_exe_args *exe_args)
 	int		res;
 	char	old_pwd[2048];
 	char	new_pwd[2048];
-	char	*error;
 
 	getcwd(old_pwd, 2048);
 	if (!exe_args->args[0])
@@ -40,12 +47,14 @@ int		exe_cd(t_exe_args *exe_args)
 		add_variable_to_list(&exe_args->variables, "?", "1", 0, 0);
 		write_error_message("minishell: cd: ", exe_args->args[0], strerror(errno)); //TODO доработать ошибки
 	}
-	else
+	else if (!res)
 	{
 		getcwd(new_pwd, 2048);
 		change_env_value(old_pwd, "OLDPWD", &exe_args->env);
 		change_env_value(new_pwd, "PWD", &exe_args->env);
 		add_variable_to_list(&exe_args->variables, "?", "0", 0, 0);
 	}
+	else if (res < -1)
+		add_variable_to_list(&exe_args->variables, "?", "1", 0, 0);
 	return (1);
 }
