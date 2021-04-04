5d5abd1309429fb45975d755154e6d0643a7df7f
Ashleigh Kasha <akasha@va-b5.kzn.21-school.ru>
2021-04-04 19:15:12 +0300

    поменял fd и file из redirect на fd[2] из структуры exec_args

--- src/operators/redirect.c
+++ src/operators/redirect.c
@@ -6,7 +6,7 @@
 /*   By: akasha <akasha@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/23 14:10:16 by akasha            #+#    #+#             */
-/*   Updated: 2021/04/04 19:09:28 by akasha           ###   ########.fr       */
+/*   Updated: 2021/04/04 19:12:49 by akasha           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,35 +33,33 @@ void	add_args(char ***exec_args, t_exe_info	*exe_info_next)
 
 void	open_and_write_to_file(t_exe_info *next, t_exe_info *original, t_exe_args *exec_args, t_exe_info *current)
 {
-	int			file;
 	int			oldstdout;
 	char		*bin_exe_path;
 	char		**next_arg_arr;
-	int			fd;
 
 	next_arg_arr = ft_split(next->args, ' ');
-	fd = 1;
+	exec_args->fd[1] = 1;
 	if (current->oper_exe_func == exe_oper_redirect)
-		file = open(next_arg_arr[0], O_CREAT | O_TRUNC | O_RDWR | O_APPEND, S_IREAD | S_IWRITE | S_IRGRP | S_IROTH);//TODO обработать ошибку
+		exec_args->fd[0] = open(next_arg_arr[0], O_CREAT | O_TRUNC | O_RDWR | O_APPEND, S_IREAD | S_IWRITE | S_IRGRP | S_IROTH);//TODO обработать ошибку
 	else if (current->oper_exe_func == exe_oper_double_redirect)
-		file = open(next_arg_arr[0], O_CREAT | O_RDWR | O_APPEND, S_IREAD | S_IWRITE | S_IRGRP | S_IROTH);//TODO обработать ошибку
+		exec_args->fd[0] = open(next_arg_arr[0], O_CREAT | O_RDWR | O_APPEND, S_IREAD | S_IWRITE | S_IRGRP | S_IROTH);//TODO обработать ошибку
 	else if (current->oper_exe_func == exe_oper_reverse_redirect)
 	{
-		file = open(next_arg_arr[0], O_RDWR);
-		fd = 0;
+		exec_args->fd[0] = open(next_arg_arr[0], O_RDWR);
+		exec_args->fd[1] = 0;
 	}
 	else
 		return ;
-	if (file == -1)
+	if (exec_args->fd[0] == -1)
 		write_error_message("minishell: ", next_arg_arr[0], strerror(errno));
 	if (next->oper_exe_func != exe_oper_redirect &&
 			next->oper_exe_func != exe_oper_double_redirect)
 	{
-		if (file && file != -1)
+		if (exec_args->fd[0] && exec_args->fd[0] != -1)
 		{
 			bin_exe_path = search(exec_args->args[0], get_env_param("PATH", exec_args->env));
-			oldstdout = dup(fd);
-			dup2(file, fd);
+			oldstdout = dup(exec_args->fd[1]);
+			dup2(exec_args->fd[0], exec_args->fd[1]);
 			if (original->exe_function)
 				original->exe_function(exec_args);
 			else if (bin_exe_path)
@@ -69,14 +67,14 @@ void	open_and_write_to_file(t_exe_info *next, t_exe_info *original, t_exe_args *
 			else if (!exec_args->args[0]);
 			else
 			{
-				dup2(oldstdout, fd);
+				dup2(oldstdout, exec_args->fd[1]);
 				unknown_command(exec_args);
 			}
-			dup2(oldstdout, fd);
+			dup2(oldstdout, exec_args->fd[1]);
 			free(bin_exe_path);
 		}
 	}
-	close(file);
+	close(exec_args->fd[0]);
 	free_2d_arr(next_arg_arr);
 }
 
