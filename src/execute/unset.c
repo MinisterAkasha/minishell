9e0f1650b8389cdea63a8e2f606427b62c43ea8a
Ashleigh Kasha <akasha@va-b5.kzn.21-school.ru>
2021-04-08 14:30:32 +0300

    норма в unset

--- src/execute/unset.c
+++ src/execute/unset.c
@@ -6,7 +6,7 @@
 /*   By: akasha <akasha@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/18 17:56:27 by akasha            #+#    #+#             */
-/*   Updated: 2021/04/08 12:56:27 by akasha           ###   ########.fr       */
+/*   Updated: 2021/04/08 14:24:20 by akasha           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -69,6 +69,30 @@ void	set_unset_status_var(t_exe_args *exe_arg)
 	add_variable(&exe_arg->variables, create_var("?", "0", 0, 0));
 }
 
+void	unset_var(t_exe_args *exe_arg, int i)
+{
+	t_variable	*var;
+	char		**env_copy;
+
+	if (validate_var_name(exe_arg->args[i]))
+	{
+		var = find_variable(exe_arg->variables, exe_arg->args[i]);
+		if (var)
+			ft_lstdelone_var(&exe_arg->variables, delete_var, var);
+		if (get_env_param(exe_arg->args[i], exe_arg->env_init))
+		{
+			env_copy = copy_2d_arr(exe_arg->env_init);
+			free_2d_arr(exe_arg->env_init);
+			exe_arg->env_init = remove_param_from_2d_arr(env_copy,
+				exe_arg->args[i]);
+			free_2d_arr(env_copy);
+		}
+	}
+	else
+		write_error("minishell: unset: '",
+			exe_arg->args[i], "': not a valid identifier");
+}
+
 int		exe_unset(t_exe_args *exe_arg)
 {
 	int			i;
@@ -78,21 +102,7 @@ int		exe_unset(t_exe_args *exe_arg)
 	i = 0;
 	while (exe_arg->args[i])
 	{
-		if (validate_var_name(exe_arg->args[i]))
-		{
-			var = find_variable(exe_arg->variables, exe_arg->args[i]);
-			if (var)
-				ft_lstdelone_var(&exe_arg->variables, delete_var, var);
-			if (get_env_param(exe_arg->args[i], exe_arg->env_init))
-			{
-				env_copy = copy_2d_arr(exe_arg->env_init);
-				free_2d_arr(exe_arg->env_init);
-				exe_arg->env_init = remove_param_from_2d_arr(env_copy, exe_arg->args[i]);
-				free_2d_arr(env_copy);
-			}
-		}
-		else
-			write_error("minishell: unset: '", exe_arg->args[i], "': not a valid identifier");
+		unset_var(exe_arg, i);
 		i++;
 	}
 	set_unset_status_var(exe_arg);
