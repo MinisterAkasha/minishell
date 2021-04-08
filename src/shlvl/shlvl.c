1fd12716f4faf34c1c46de2457323ccca3bad85a
Ashleigh Kasha <akasha@va-b5.kzn.21-school.ru>
2021-04-08 19:45:31 +0300

    норма shlvl

--- src/shlvl/shlvl.c
+++ src/shlvl/shlvl.c
@@ -6,7 +6,7 @@
 /*   By: akasha <akasha@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/13 18:19:30 by akasha            #+#    #+#             */
-/*   Updated: 2021/04/08 19:43:24 by akasha           ###   ########.fr       */
+/*   Updated: 2021/04/08 19:45:17 by akasha           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ void	shlvl(t_exe_args exe_args)
 
 	if (get_env_param("SHLVL", exe_args.env_init))
 	{
-		cur_lvl = ft_atoi(ft_strchr(get_env_param("SHLVL",exe_args.env_init),
+		cur_lvl = ft_atoi(ft_strchr(get_env_param("SHLVL", exe_args.env_init),
 			'=') + 1);
 		lvl = ft_itoa(cur_lvl + 1);
 		change_env_value(lvl, "SHLVL", &exe_args.env_init);
