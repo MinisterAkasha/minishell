8f979428785fbd7571f12fcc027ec7a0d9942fb1
Ashleigh Kasha <akasha@va-b5.kzn.21-school.ru>
2021-04-12 13:02:47 +0300

    перенес присвоение bin_path в блок if в функции execute

--- src/execute/execute.c
+++ src/execute/execute.c
@@ -6,7 +6,7 @@
 /*   By: akasha <akasha@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/09 13:22:26 by akasha            #+#    #+#             */
-/*   Updated: 2021/04/12 12:44:36 by akasha           ###   ########.fr       */
+/*   Updated: 2021/04/12 13:02:08 by akasha           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,16 +60,17 @@ int		choose_command_and_run(t_exe_info *exe_info, t_store *store,
 {
 	char		*bin_exe_path;
 
-	if (!exe_info->exe_function)
-		bin_exe_path = search(&store->exe_args,
-			get_env_param("PATH", store->exe_args.env));
 	if (exe_info->oper_exe_func &&
 		exe_info->oper_exe_func != exe_oper_semicolon)
 		run_oper_exec_function(&store->exe_args, exe_info, info);
 	else if (exe_info->exe_function)
 		exe_info->exe_function(&store->exe_args);
-	else if (bin_exe_path)
+	else if ((bin_exe_path = search(&store->exe_args,
+		get_env_param("PATH", store->exe_args.env))))
+	{
 		launch_process(store->exe_args, bin_exe_path);
+		free(bin_exe_path);
+	}
 	else if (!ft_strlen(exe_info->args))
 	{
 		free(bin_exe_path);
@@ -77,8 +78,6 @@ int		choose_command_and_run(t_exe_info *exe_info, t_store *store,
 	}
 	else if (get_env_param("PATH", store->exe_args.env))
 		unknown_command(&store->exe_args);
-	if (!exe_info->exe_function)
-		free(bin_exe_path);
 	return (0);
 }
 
